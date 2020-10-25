e7fa3eb90c06d6b1493d8c5d8c1e849a4a478deb
Sandeep-Das-og <das2106sandeep@gmail.com>
2020-10-25 21:18:45 +0530

    Updated the layout of login UI

--- app/src/main/java/com/example/buysd/userTabActivity.java
+++ app/src/main/java/com/example/buysd/userTabActivity.java
@@ -47,6 +47,8 @@ public class userTabActivity extends AppCompatActivity {
             }
         });
 
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
     }
 
 
